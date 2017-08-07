e9ebb9688714b3772be4ce3add906518ca4cdd00
Amy Sun <yimingsun24@gmail.com>
2017-08-07 12:36:37 -0700

    updated classify_test_data with new classifier

--- classify_test_data.py
+++ classify_test_data.py
@@ -5,7 +5,7 @@ Created on Sat Aug 05 21:40:42 2017
 @author: Yiming
 """
 
-from classifier import classify_one
+from classifier import classify
 import pandas as pd
 from hand import Hand
 import os
@@ -20,11 +20,11 @@ hands = []
 for index, row in df.iterrows():
 #    print row['id']
     myhand = Hand(row)
-    classify_one(myhand)
+    classify(myhand)
     hands.append([row['id'], myhand.assigned_class])
 
 print 'data classified and loaded'
 dfw = pd.DataFrame(hands, columns = ['id','hand'])
 
 print 'write to csv'
-dfw.to_csv('classifyTest.csv', sep = ',', index=False)
+dfw.to_csv('classifyTest2.csv', sep = ',', index=False)
